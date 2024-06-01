b074364b52c11d06e56722423312cb020882e928
aaru2599 <arvindmandre2@gmail.com>
2024-06-01 20:38:06 +0530

    Loader fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
 import React, { Suspense } from "react";
-import { Audio, Circles } from "react-loader-spinner";
+import { Circles } from "react-loader-spinner";
 
 const LazyMainPage = React.lazy(() =>
-  import('./Components/Navigation/MainPage')
+  import("./Components/Navigation/MainPage")
 );
 
 const App = () => {
   return (
-   <Suspense fallback={<div className="d-flex justify-content-center align-items-center  fs-3 fw-bold" style={{height:"100vh",color:"#0d6efd"}}>Loading...<Circles
-   height="80"
-   width="80"
-   color="#0d6efd"
-   ariaLabel="circles-loading"
-   wrapperStyle={{}}
-   wrapperClass=""
-   visible={true}
-   /></div>}>
-<LazyMainPage/>
-   </Suspense>
+    <Suspense
+      fallback={
+        <div
+          className="d-flex justify-content-center align-items-center gap-4  fs-3 fw-bold"
+          style={{ height: "100vh", color: "#0d6efd" }}
+        >
+         <div> Loading</div>
+          <Circles
+            height="50"
+            width="50"
+            color="#0d6efd"
+            ariaLabel="circles-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      }
+    >
+      <LazyMainPage />
+    </Suspense>
   );
 };
 
